e91d9ba59e5b60a3c751f8725194a21b5a170bc4
David <david.maldonado.95@outlook.com>
2024-08-05 02:30:05 -0500

    Added modularity to assist with toggling features. Fixed Brace Matching, Indentation Strategy, and Code Folding. Enabled toggling of the "Code Editing" related features.

--- CodePal/CodeEditor/BraceMatchingStrategy.cs
+++ CodePal/CodeEditor/BraceMatchingStrategy.cs
@@ -2,78 +2,88 @@
 using Avalonia.Input;
 using AvaloniaEdit;
 using AvaloniaEdit.Document;
+using AvaloniaEdit.Indentation.CSharp;
 
 
 namespace CodePal.CodeEditor;
 
-public class BraceMatchingStrategy
-{
-    public static void HandleBraceMatching(TextEditor textEditor, TextInputEventArgs e)
+    public class BraceMatchingStrategy
     {
-        var document = textEditor.Document;
-        var inputText = e.Text;
-
-        switch (inputText)
+        public static void HandleBraceMatching(TextEditor textEditor, TextInputEventArgs e)
         {
-            case "{":
-                InsertMatchingBrace(textEditor, "}");
-                break;
-            case "[":
-                InsertMatchingBrace(textEditor, "]");
-                break;
-            case "(":
-                InsertMatchingBrace(textEditor, ")");
-                break;
-            case "<":
-                InsertMatchingBrace(textEditor, ">");
-                break;
+            var document = textEditor.Document;
+            var inputText = e.Text;
+
+            switch (inputText)
+            {
+                case "{":
+                    InsertMatchingBrace(textEditor, "}");
+                    break;
+                case "[":
+                    InsertMatchingBrace(textEditor, "]");
+                    break;
+                case "(":
+                    InsertMatchingBrace(textEditor, ")");
+                    break;
+                case "<":
+                    InsertMatchingBrace(textEditor, ">");
+                    break;
+            }
+        }
+
+        private static void InsertMatchingBrace(TextEditor textEditor, string closingBrace)
+        {
+            var document = textEditor.Document;
+            var caretOffset = textEditor.CaretOffset;
+
+            // Insert the closing brace
+            document.Insert(caretOffset, closingBrace);
+
+            // Move caret inside the braces
+            textEditor.CaretOffset = caretOffset;
+
+            if (closingBrace == "}")
+            {
+                // Insert new lines
+                document.Insert(caretOffset, "\n");
+                document.Insert(caretOffset + 1, "\n");
+
+                // Move caret to the first new line
+                textEditor.CaretOffset = caretOffset + 1;
+
+                // Apply the indentation strategy
+                var indentationStrategy = textEditor.TextArea.IndentationStrategy as CSharpIndentationStrategy;
+                if (indentationStrategy != null)
+                {
+                    // Indent the line where the caret is currently located
+                    var currentLine = document.GetLineByOffset(textEditor.CaretOffset);
+                    indentationStrategy.IndentLine(document, currentLine);
+
+                    // Move the caret to the new indented position
+                    var newCaretOffset = currentLine.Offset + GetWhitespaceAfterOffset(currentLine, document);
+                    textEditor.CaretOffset = newCaretOffset;
+
+                    // Indent the closing brace line as well
+                    var closingBraceLine = document.GetLineByOffset(textEditor.CaretOffset + 1);
+                    if (closingBraceLine != null)
+                    {
+                        indentationStrategy.IndentLine(document, closingBraceLine);
+                    }
+                }
+            }
+        }
+
+        private static int GetWhitespaceAfterOffset(DocumentLine line, TextDocument document)
+        {
+            var lineText = document.GetText(line.Offset, line.Length);
+            var whitespaceCount = 0;
+            foreach (var ch in lineText)
+            {
+                if (char.IsWhiteSpace(ch))
+                    whitespaceCount++;
+                else
+                    break;
+            }
+            return whitespaceCount;
         }
     }
-
-    private static void InsertMatchingBrace(TextEditor textEditor, string closingBrace)
-    {
-        var document = textEditor.Document;
-        var caretOffset = textEditor.CaretOffset;
-
-        // Insert the closing brace
-        document.Insert(caretOffset, closingBrace);
-
-        // Move caret inside the braces
-        textEditor.CaretOffset = caretOffset;
-
-        if (closingBrace == "}")
-        {
-            // Insert new lines
-            document.Insert(caretOffset, "\n");
-            document.Insert(caretOffset + 1, "\n");
-
-            // Get the current line and the new lines
-            var currentLine = document.GetLineByOffset(caretOffset - 1);
-            var nextLine = document.GetLineByOffset(caretOffset + 1);
-
-            // Calculate the indentation based on the current line
-            var indentation = GetIndentationForLine(document, currentLine);
-
-            // Move caret to the first new line and apply the calculated indentation
-            textEditor.CaretOffset = nextLine.Offset;
-            document.Insert(nextLine.Offset, indentation);
-            textEditor.CaretOffset = nextLine.Offset + indentation.Length;
-        }
-    }
-
-    private static string GetIndentationForLine(TextDocument document, DocumentLine line)
-    {
-        var lineText = document.GetText(line.Offset, line.Length);
-        var indentLevel = 0;
-
-        // Count the number of opening and closing braces to determine the indentation level
-        foreach (char c in lineText)
-        {
-            if (c == '{') indentLevel++;
-            if (c == '}') indentLevel--;
-        }
-
-        // Return a string with the appropriate number of spaces for indentation
-        return new string(' ', Math.Max(indentLevel, 0) * 4); // Adjust 4 to your preferred indentation size
-    } 
-}
--- CodePal/MainWindow.axaml.cs
+++ CodePal/MainWindow.axaml.cs
@@ -1,9 +1,11 @@
 using System;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using AvaloniaEdit;
 using AvaloniaEdit.Document;
 using AvaloniaEdit.Folding;
+using AvaloniaEdit.Indentation.CSharp;
 using AvaloniaEdit.TextMate;
 using TextMateSharp.Grammars;
 using CodePal.CodeEditor;
@@ -12,37 +14,103 @@ namespace CodePal;
 
 public partial class MainWindow : Window
 {
+    // Declarations to be used throughout the MainWindow class.
+    private TextEditor _textEditor;
+    private RegistryOptions _registryOptions;
+    private TextMate.Installation _textMateInstallation;
+    private FoldingManager _foldingManager;
+    private CodeFoldingStrategy _foldingStrategy;
+    private bool IsHighlightingEnabled = true;
+    private bool IsCodeFoldingEnabled = true;
+    private bool IsBraceMatchingEnabled = true;
+
     public MainWindow()
     {
+        // Initializaition of components, editor, and main Event Handler for Syntax Highlighting toggle.
         InitializeComponent();
         InitializeEditor();
+        ToggleSyntaxHighlighting.IsChecked = true; // Enable by default
+        ToggleSyntaxHighlighting.IsCheckedChanged += ToggleSyntaxHighlighting_IsCheckedChanged;
     }
+
     private void InitializeEditor()
     {
-        //First of all you need to have a reference for your TextEditor for it to be used inside AvaloniaEdit.TextMate project.
-        var _textEditor = this.FindControl<TextEditor>("Editor");
-
-        //Here we initialize RegistryOptions with the theme we want to use.
-        var  _registryOptions = new RegistryOptions(ThemeName.DarkPlus);
-
-        //Initial setup of TextMate.
-        var _textMateInstallation = _textEditor.InstallTextMate(_registryOptions);
-
-        //Here we are getting the language by the extension and right after that we are initializing grammar with this language.
-        //And that's all 😀, you are ready to use AvaloniaEdit with syntax highlighting!
+        // Find the editor created in the window xaml.
+        _textEditor = this.FindControl<TextEditor>("Editor");
+        // Set the theme to be used.
+        _registryOptions = new RegistryOptions(ThemeName.DarkPlus);
+        // Install TextMate
+        _textMateInstallation = _textEditor.InstallTextMate(_registryOptions);
+        // Set the language for syntax highlighting.
         _textMateInstallation.SetGrammar(_registryOptions.GetScopeByLanguageId(_registryOptions.GetLanguageByExtension(".cs").Id));
-        
-        // Code Folding
-        var foldingManager = FoldingManager.Install(_textEditor.TextArea);
-        var foldingStrategy = new CodeFoldingStrategy();
-        _textEditor.TextChanged += (s, e) => foldingStrategy.UpdateFoldings(foldingManager, ((TextEditor)s).Document);
-        
-        // Handle TextEntered event for brace matching
-        _textEditor.TextArea.TextEntered += (sender, e) =>
+        // Set the Indentation Strategy to be used. For now we are using the included CSharp Indentation Strategy.
+        _textEditor.TextArea.IndentationStrategy = new CSharpIndentationStrategy(_textEditor.Options);
+        // Install the Code Folding Manager
+        _foldingManager = FoldingManager.Install(_textEditor.TextArea);
+        // Set the Folding Strategy to our CodeFoldingStrategy in /CodeEditor
+        _foldingStrategy = new CodeFoldingStrategy();
+        // Handle the TextChanged event for enabling/disabling code folding.
+        _textEditor.TextChanged += OnTextChanged;
+        // Handle the TextEntered even for enabling/disabling brace matching.
+        _textEditor.TextArea.TextEntered += OnTextEntered;
+    }
+
+    // Event Handler for OnTextChanged
+    private void OnTextChanged(object sender, EventArgs e)
+    {
+        // Check that IsCodeFolding is equal to true. If it is, update the locations where code can be folded and display it.
+        if (IsCodeFoldingEnabled)
+        {
+            _foldingStrategy.UpdateFoldings(_foldingManager, _textEditor.Document);
+        }
+    }
+
+    // Event handler for OnTextEntered
+    private void OnTextEntered(object sender, TextInputEventArgs e)
+    {
+        // Check that IsBraceMatching is equal to true. If it is, begin to match braces as typed by the user.
+        if (IsBraceMatchingEnabled)
         {
             BraceMatchingStrategy.HandleBraceMatching(_textEditor, e);
-        };
+        }
     }
-    
-}
 
+    // Event Handler for IsCheckChanged on the ToggleSyntaxHighlighting button.
+    private void ToggleSyntaxHighlighting_IsCheckedChanged(object? sender, EventArgs e)
+    {
+        if (ToggleSyntaxHighlighting.IsChecked == true)
+        {
+            // Set the syntax language.
+            _textMateInstallation.SetGrammar(_registryOptions.GetScopeByLanguageId(_registryOptions.GetLanguageByExtension(".cs").Id));
+            // Install the Folding Manager
+            _foldingManager = FoldingManager.Install(_textEditor.TextArea);
+            // Assign the TextChanged event.
+            _textEditor.TextChanged += OnTextChanged;
+            // Assign the TextEntered event.
+            _textEditor.TextArea.TextEntered += OnTextEntered;
+            // Set the Indentation Strategy,
+            _textEditor.TextArea.IndentationStrategy = new CSharpIndentationStrategy(_textEditor.Options);
+            // Set our boolean checks as needed.
+            IsHighlightingEnabled = true;
+            IsCodeFoldingEnabled = true;
+            IsBraceMatchingEnabled = true;
+        }
+        else
+        {
+            // Set the syntax language to null.
+            _textMateInstallation.SetGrammar(null);
+            // Uninstall the Folding Manager.
+            FoldingManager.Uninstall(_foldingManager);
+            // Unassign the TextChanged event.
+            _textEditor.TextChanged -= OnTextChanged;
+            // Unassign the TextEntered event.
+            _textEditor.TextArea.TextEntered -= OnTextEntered;
+            // Set the Indentation Strategy to null.
+            _textEditor.TextArea.IndentationStrategy = null;
+            // Set our boolean checks as needed.
+            IsHighlightingEnabled = false;
+            IsCodeFoldingEnabled = false;
+            IsBraceMatchingEnabled = false;
+        }
+    }
+}
